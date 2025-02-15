d0a9d72417b1ccb69f6cf7378df8f0b27927a362
justpanca <sajapanca6@gmail.com>
2025-02-15 21:46:05 +0700

    handling cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://frontend-tokobuku.vercel.app'], 
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
